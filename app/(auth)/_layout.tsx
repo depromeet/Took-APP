bdae57d9f93cf3f1facdb3b1d3884fa916a8e04f
jangwonyoon <wkddnjs7310@naver.com>
2025-04-13 13:30:41 +0900

    ✨ Feat: aab 파일 추출 빌드시스템 구축 및 화면 노치 바 스타일 수정

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,6 +1,27 @@
 import React from "react";
 import { Slot } from "expo-router";
+import { StyleSheet } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+  },
+  statusBar: {
+    backgroundColor: "#000",
+  },
+});
 
 export default function AuthLayout() {
-  return <Slot />;
+  return (
+    <SafeAreaView style={styles.container} edges={[]}>
+      <StatusBar
+        style="light"
+        backgroundColor={styles.statusBar.backgroundColor}
+      />
+      <Slot />
+    </SafeAreaView>
+  );
 }
